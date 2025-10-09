c1cc31cfb68c7e6c7d100b8e4e5ce5604c3af7e9
24522065 <24522065@gm.uit.edu.vn>
2025-10-09 09:31:04 +0700

    Update Bai05.cs

--- Bai05.cs
+++ Bai05.cs
@@ -1,31 +1,51 @@
-﻿using System;
+using System;
 
-namespace _5
+namespace Bai05
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write(" - Date: ");
-            int day = int.Parse(Console.ReadLine());
+            int day, month, year;
+            DateTime date;
 
-            Console.Write(" - Month: ");
-            int month = int.Parse(Console.ReadLine());
-
-            Console.Write(" - Year: ");
-            int year = int.Parse(Console.ReadLine());
-
-            try
+            while (true)
             {
-                DateTime date = new DateTime(year, month, day);
+                Console.Write(" - Date: ");
+                if (!int.TryParse(Console.ReadLine(), out day) || day <= 0 || day>31)
+                {
+                    Console.WriteLine("Ngay khong hop le. Vui long nhap lai!\n");
+                    continue;
+                }
 
-                Console.WriteLine($"  {day}/{month}/{year} is {date.DayOfWeek}");
+                Console.Write(" - Month: ");
+                if (!int.TryParse(Console.ReadLine(), out month) || month < 1 || month > 12)
+                {
+                    Console.WriteLine("Thang khong hop le. Vui long nhap lai!\n");
+                    continue;
+                }
+
+                Console.Write(" - Year: ");
+                if (!int.TryParse(Console.ReadLine(), out year) || year <= 0)
+                {
+                    Console.WriteLine("Nam khong hop le. Vui long nhap lai!\n");
+                    continue;
+                }
+
+                try
+                {
+                    date = new DateTime(year, month, day);
+                    break; 
+                }
+                catch
+                {
+                    Console.WriteLine($"Ngay {day}/{month}/{year} khong hop le! Vui long nhap lai.\n");
+                }
             }
-            catch
-            {
-                Console.WriteLine($"  {day}/{month}/{year} is invalid");
-            }
+
+            Console.WriteLine($"\nNgay {day}/{month}/{year} la {date.DayOfWeek}.");
         }
     }
 }
+
 

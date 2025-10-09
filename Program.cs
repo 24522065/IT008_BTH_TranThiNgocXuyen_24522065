da4ed03ae0e631bcded548ec3a8c8053154eeda0
24522065 <24522065@gm.uit.edu.vn>
2025-10-09 07:49:07 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace _02
+{
+    class Program
+    {
+        static bool SoNguyenTo(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i * i <= n; i++)
+                if (n % i == 0) return false;
+            return true;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Nhap so nguyen duong n: ");
+            int n = int.Parse(Console.ReadLine());
+            int tong = 0;
+            for (int i = 2; i < n; i++)
+            {
+                if (SoNguyenTo(i))
+                {
+                    tong += i;
+                }
+            }
+            Console.WriteLine("Tong cac so nguyen to <n: " + tong);
+        }
+    }
+}

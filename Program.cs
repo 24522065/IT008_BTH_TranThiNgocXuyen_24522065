be2a27bb99ae9f811e80dd59990f2416d28fb9be
24522065 <24522065@gm.uit.edu.vn>
2025-10-09 07:51:29 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bai05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" - Date: ");
+            int day = int.Parse(Console.ReadLine());
+
+            Console.Write(" - Month: ");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.Write(" - Year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            try
+            {
+                DateTime date = new DateTime(year, month, day);
+
+                Console.WriteLine($"  {day}/{month}/{year} is {date.DayOfWeek}");
+            }
+            catch
+            {
+                Console.WriteLine($"  {day}/{month}/{year} is invalid");
+            }
+        }
+    }
+}
